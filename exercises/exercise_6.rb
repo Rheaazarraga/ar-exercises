35a1af8b65ae16f8ded3b6e3beb42f97f0b07c4b
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-11 23:12:50 +0000

    Create 3 employees each for store1 & store2, load data into employees, and modify code in store and employee model classes for one to many associations

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -6,6 +6,25 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 
 puts "Exercise 6"
-puts "----------"
+puts "One-to-many association"
 
 # Your code goes here ...
+
+# Add the following code directly inside the Store model (class): has_many :employees
+# Add the following code directly inside the Employee model (class): belongs_to :store
+# Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+# Create some more employees using the create method. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...)
+
+Employee.create(store_id: 1, first_name: "Bita", last_name: "Bean", hourly_rate: 100)
+Employee.create(store_id: 1, first_name: "Nikocado", last_name: "Avocado", hourly_rate: 64)
+Employee.create(store_id: 1, first_name: "Charlie", last_name: "White", hourly_rate: 95)
+
+Employee.create(store_id: 2, first_name: "Justina", last_name: "Lamb", hourly_rate: 75)
+Employee.create(store_id: 2, first_name: "Besquiddle", last_name: "Diddle", hourly_rate: 69)
+Employee.create(store_id: 2, first_name: "Chingle", last_name: "Chungle", hourly_rate: 80)
+
+puts "Count store 1 employees:"
+puts @store1.employees.count
+
+puts "Count store 2 employees:"
+puts @store2.employees.count
