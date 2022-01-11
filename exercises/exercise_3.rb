5f22989f57407791a731a4b01d344db1594ca307
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-11 04:28:07 +0000

    Load store3 into an instance variable, then destroy it and print the store count

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -3,6 +3,16 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 
 puts "Exercise 3"
-puts "----------"
+puts "Delete the third store"
 
 # Your code goes here ...
+
+# Load the third store (into @store3) as you did the other two before
+@store3 = Store.find 3
+
+# Using Active Record's destroy method, delete the store from the database
+@store3.destroy
+
+# Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
+puts "After destroying store 3, store count:"
+puts Store.count
